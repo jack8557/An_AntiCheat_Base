cee2c91e2a85ef455a979b05c5652a8f4f6e870d
jack <jackkowatch@icloud.com>
2020-10-07 12:56:39 +0200

    Added more functions to CollisionManager.kt

--- src/main/kotlin/me/godead/anticheat/users/impl/CollisionManager.kt
+++ src/main/kotlin/me/godead/anticheat/users/impl/CollisionManager.kt
@@ -1,5 +1,6 @@
 package me.godead.anticheat.users.impl
 
+import me.godead.anticheat.users.BoundingBox
 import me.godead.anticheat.users.User
 import me.godead.anticheat.utils.XMaterial
 
@@ -18,5 +19,42 @@ class CollisionManager(val user: User) {
     fun touchingAny(vararg materials: XMaterial) = materials.any { touchingAny(it) }
 
     fun touchingAll(vararg materials: XMaterial) = materials.any { touchingAll(it) }
+
+
+    private fun touchingAny(boundingBox: BoundingBox, material: XMaterial) = boundingBox.checkBlocksAny(
+        user.player.world
+    ) { XMaterial: XMaterial -> XMaterial == material }
+
+
+    private fun touchingAll(boundingBox: BoundingBox, material: XMaterial) = boundingBox.checkBlocks(
+        user.player.world
+    ) { XMaterial: XMaterial -> XMaterial == material }
+
+
+    fun touchingAny(boundingBox: BoundingBox, vararg materials: XMaterial) =
+        materials.any { touchingAny(boundingBox, it) }
+
+    fun touchingAll(boundingBox: BoundingBox, vararg materials: XMaterial) =
+        materials.any { touchingAll(boundingBox, it) }
+
+    fun touchingAny(height: Double, vararg materials: XMaterial) = materials.any {
+        touchingAny(
+            BoundingBox(
+                user.player.location.x,
+                height,
+                user.player.location.z
+            ), it
+        )
+    }
+
+    fun touchingAll(height: Double, vararg materials: XMaterial) = materials.any {
+        touchingAll(
+            BoundingBox(
+                user.player.location.x,
+                height,
+                user.player.location.z
+            ), it
+        )
+    }
 
 }

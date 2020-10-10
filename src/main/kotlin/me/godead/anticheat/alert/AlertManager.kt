45b3b150adf7d26b7b653ad154769c51d6fc6457
jack <jackkowatch@icloud.com>
2020-10-10 12:09:41 +0200

    Made all messages customizable

--- src/main/kotlin/me/godead/anticheat/alert/AlertManager.kt
+++ src/main/kotlin/me/godead/anticheat/alert/AlertManager.kt
@@ -9,21 +9,34 @@ import org.bukkit.Bukkit
 
 open class AlertManager {
 
-    private val alertMessage: String = AntiCheatManager.defaultConfig.getOrSet("Alerts.Message","&8[&c&l!&8] &c%player% &7failed &6%check_name% %check_type% &ex%vl%") as String
-    private val alertPermission: String = AntiCheatManager.defaultConfig.getOrSet("Alerts.Permission","anticheat.alerts") as String
+    private val alertMessage: String = AntiCheatManager.defaultConfig.getOrSet(
+        "Alerts.Message",
+        "&8[&c&l!&8] &c%player% &7failed &6%check_name% %check_type% &ex%vl%"
+    ) as String
 
-    private val punishCommands: List<String> = AntiCheatManager.defaultConfig.getOrSet("Punish.Commands", listOf("kick %player% &7You have been kicked for &c%check_name%")) as List<String>
+    private val alertPermission: String =
+        AntiCheatManager.defaultConfig.getOrSet("Alerts.Permission", "anticheat.alerts") as String
+
+    private val punishCommands: List<String> = AntiCheatManager.defaultConfig.getOrSet(
+        "Punish.Commands",
+        listOf("kick %player% &7You have been kicked for &c%check_name%")
+    ) as List<String>
+
+    private val punishBroadcastEnabled: Boolean = AntiCheatManager.defaultConfig.getOrSet(
+        "Punish.Broadcast.Enabled",
+        true
+    ) as Boolean
+
+    private val punishBroadcastMessage: String = AntiCheatManager.defaultConfig.getOrSet(
+        "Punish.Broadcast.Message",
+        "&8[&c&l!&8] &c%player% &7has been removed from the server for cheating."
+    ) as String
 
     open fun onFlag(user: User, check: Check) {
-        alertMessage.replace("%player%", user.player.name)
-            .replace("%check_name%", check.checkName)
-            .replace("%check_type%", check.checkType.toString())
-            .replace("%vl%", check.vl.toString())
-
         UserManager.users.parallelStream()
             .filter { it.alerts }
             .filter { it.player.hasPermission(alertPermission) }
-            .forEach { it.player.sendMessage(alertMessage.color()) }
+            .forEach { it.player.sendMessage(alertMessage.applyPlaceHolders(user, check)) }
         if (check.vl >= check.maxVL) onPunish(user, check)
     }
 
@@ -31,16 +44,22 @@ open class AlertManager {
         if (!check.punishable) return
         Bukkit.getScheduler().runTask(AntiCheatManager.plugin, Runnable {
             punishCommands.forEach {
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
-                it
-                .replace("%player%", user.player.name)
-                .replace("%check_name%", check.checkName)
-                .replace("%check_type%", check.checkType.toString())
-                .replace("%vl%", check.vl.toString())
-                .color())
+                Bukkit.dispatchCommand(
+                    Bukkit.getConsoleSender(),
+                    it.applyPlaceHolders(user, check)
+                )
             }
         })
+        if (punishBroadcastEnabled) Bukkit.broadcastMessage(punishBroadcastMessage.applyPlaceHolders(user, check))
     }
+
+    private fun String.applyPlaceHolders(user: User, check: Check) =
+        replace("%player%", user.player.name)
+            .replace("%check_name%", check.checkName)
+            .replace("%check_type%", check.checkType.toString())
+            .replace("%vl%", check.vl.toString())
+            .color()
+
 
 }
 
